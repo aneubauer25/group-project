1089579cf006e43d71c263547dfd904a8315206b
aneubauer25 <andrew_neubauer@brown.edu>
2025-03-14 14:50:39 -0400

    Update clps0950_project.m
    slight formatting changes to control summary stats. 3/14

--- clps0950_project.m
+++ clps0950_project.m
@@ -1,5 +1,6 @@
 summary(data_Control)
 %%Recreating Stats for Table 1%%
+
 %%Mean, STD, and Median of ToM Tests%%
 mean_fscore = mean(data_Control.fscore);
 mean_rscore = mean(data_Control.rscore);
@@ -7,9 +8,11 @@ std_fscore = std(data_Control.fscore);
 std_rscore = std(data_Control.rscore);
 median_fscore = median(data_Control.fscore);
 median_rscore = median(data_Control.rscore);
+
 %%Mean & STD of Age Demographics%%
 mean_age = mean(data_Control.a);
 std_age = std(data_Control.a);
+
 %%Mean & STD of Correct Responses to Emotional Facial Recognition Task%%
 mean_dcorr = mean(data_Control.dcorr);
 std_dcorr = std(data_Control.dcorr);
@@ -25,12 +28,15 @@ mean_ncorr = mean(data_Control.ncorr);
 std_ncorr = std(data_Control.ncorr);
 mean_totalcorr = mean(data_Control.totalcorr);
 std_totalcorr = std(data_Control.totalcorr);
+
 %%Creating a table similar to Table 1%%
+
 % Define Variable Categories
 Categories = {'Age (years)'; ' '; ...
               'Emotion Facial Recognition Accuracy'; 'Happy'; 'Angry'; 'Fearful'; ...
               'Sad'; 'Disgusted'; 'Neutral'; 'Total'; ' '; ...
               'ToM Test'; 'RMET-C'; 'FPRT-C'};
+
 % Control Mean ± SD
 Control = {'10.6 ± 1.8'; ' '; ...
            ' '; '5.8 ± 0.5'; '5.2 ± 0.9'; '4.9 ± 1.5'; '4.1 ± 1.7'; '5.5 ± 0.9'; '5.5 ± 1.0'; '31.0 ± 3.3'; ' '; ...
